21703dee27b5bb431532b2dba9c43dfa5410d571
Admin <tjuthww@163.com>
2017-06-15 16:27:55 +0800

    no message

--- WeiBo/WeiBo/Classes/View/Main/HWBBaseViewController.swift
+++ WeiBo/WeiBo/Classes/View/Main/HWBBaseViewController.swift
@@ -12,8 +12,9 @@ class HWBBaseViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.cz_random()
+        
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +22,5 @@ class HWBBaseViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
