db6c1b6904038eeb9d237d7dbb945e5f9679d7d7
Admin <tjuthww@163.com>
2017-06-15 17:14:23 +0800

    no message

--- WeiBo/WeiBo/Classes/View/Main/HWBMainTabBarVC.swift
+++ WeiBo/WeiBo/Classes/View/Main/HWBMainTabBarVC.swift
@@ -22,3 +22,35 @@ class HWBMainTabBarVC: UITabBarController {
     
 
 }
+
+//类似于OC中的分类，swift中切分代码，相近代码的函数放在一个extension便于代码维护
+//和OC的分类一样，不能定义属性
+//设置界面
+extension HWBMainTabBarVC
+{
+    private func setupChildController()
+    {
+        let array = [
+            ["clsName":"","title":"","imageName":""],
+        
+        ]
+        
+        var arrayM = [UIViewController]()
+        
+    }
+    //创建一个控制器
+    private func controller(dict: [String : String]) -> UIViewController {
+        guard let clsName = dict["clsName"],let title = dict["title"],let imageName = dict["imageName"],let cls = NSClassFromString(Bundle.main.namespace + "." + clsName) as? UIViewController.Type else{
+            
+            return UIViewController()
+        }
+        
+        //创建视图控制器
+        let vc = cls.init()
+        vc.title = title;
+        let nav = HWBNavigationVC.init(rootViewController: vc)
+        
+        return nav
+        
+    }
+}
