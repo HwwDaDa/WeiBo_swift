ea5286f39b2b2809de09455b252617891edec336
Admin <tjuthww@163.com>
2017-06-15 15:18:23 +0800

    添加框架

---
+++ WeiBo/WeiBo/Classes/View/Home/HWBHomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HWBHomeViewController.swift
+//  WeiBo
+//
+//  Created by Admin on 2017/6/15.
+//  Copyright © 2017年 Admin. All rights reserved.
+//
+
+import UIKit
+
+class HWBHomeViewController: HWBBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
---
+++ WeiBo/WeiBo/Classes/View/Main/HWBNavigationVC.swift
@@ -0,0 +1,26 @@
+//
+//  HWBNavigationVC.swift
+//  WeiBo
+//
+//  Created by Admin on 2017/6/15.
+//  Copyright © 2017年 Admin. All rights reserved.
+//
+
+import UIKit
+
+class HWBNavigationVC: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
