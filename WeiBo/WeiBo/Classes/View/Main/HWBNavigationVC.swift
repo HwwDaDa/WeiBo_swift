f1407ad9b91b1b5276f0437de8a47ac049924bdb
Admin <tjuthww@163.com>
2017-06-21 22:12:29 +0800

    重写push方法

--- WeiBo/WeiBo/Classes/View/Main/HWBNavigationVC.swift
+++ WeiBo/WeiBo/Classes/View/Main/HWBNavigationVC.swift
@@ -10,14 +10,29 @@ import UIKit
 
 class HWBNavigationVC: UINavigationController {
 
+    //重写push方法,所有的push都会调用这个方法
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        
+        //如果不是栈底控制器不隐藏
+        if childViewControllers.count > 0{
+            
+            viewController.hidesBottomBarWhenPushed = true
+        }
+        //隐藏底部的tabBar
+        
+        super.pushViewController(viewController, animated: true)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
     
 
