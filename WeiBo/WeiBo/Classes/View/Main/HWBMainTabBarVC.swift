78e4daa263c0f2866ff3a783bf988769f8984379
hwwDaDa <hwwtjut@163.com>
2017-07-04 14:04:13 +0800

    设置设备方向支持

--- WeiBo/WeiBo/Classes/View/Main/HWBMainTabBarVC.swift
+++ WeiBo/WeiBo/Classes/View/Main/HWBMainTabBarVC.swift
@@ -16,6 +16,12 @@ class HWBMainTabBarVC: UITabBarController {
         setupChildController()
         setupComposeButton()
     }
+    
+//    - 支持的设法方向,一般时候默认支持多个方向，因为后面加视频播放的页面，这个页面是支持横屏的
+//    - 这个方法是控制器定义的，在设置支持方向之后当前的控制器和子控制器都会遵守这个方向，如果播放视频是通过modal出来的
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask{
+        return .portrait
+    }
 
     //private只能在本类使用，
     //@bojc，在运行时通过OC的消息机制被使用
