64778cbeeb1be1204c06d24cc959216cccc8bfbd
hwwDaDa <hwwtjut@163.com>
2017-07-04 14:27:08 +0800

    抽取导航栏的代码

--- WeiBo/WeiBo/Classes/View/Main/HWBBaseViewController.swift
+++ WeiBo/WeiBo/Classes/View/Main/HWBBaseViewController.swift
@@ -9,14 +9,23 @@
 import UIKit
 
 class HWBBaseViewController: UIViewController {
-
+//  表格视图，因此不能用懒加载
+    var tableView: UITableView?
+    
+    lazy var navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: UIScreen.cz_screenWidth(), height: 64))
+    lazy var navItem = UINavigationItem()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupUI()
     }
 
-    
+    override var title: String?{
+        didSet{
+            navItem.title = title
+        }
+    }
 
 }
 
@@ -25,5 +34,20 @@ class HWBBaseViewController: UIViewController {
 extension HWBBaseViewController{
     func setupUI(){
         view.backgroundColor = UIColor.cz_random()
+        
+        
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar(){
+        //添加导航条
+        view.addSubview(navigationBar)
+        
+        navigationBar.items = [navItem]
+        
+        navigationBar.barTintColor = UIColor.cz_color(withHex: 0xF6F6F6)
+        navigationBar.titleTextAttributes = [NSForegroundColorAttributeName:UIColor.darkGray]
     }
 }
+
+
