483a10255d233b4de9d52df613ed298159a462c5
hwwDaDa <hwwtjut@163.com>
2017-07-04 14:55:54 +0800

    隔离代码

--- WeiBo/WeiBo/Classes/View/Main/HWBBaseViewController.swift
+++ WeiBo/WeiBo/Classes/View/Main/HWBBaseViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+//OC重支持多继承码，不支持，答案是使用协议替代
+//swift的这种方法类似于多继承，这就是不同于OC的地方
 class HWBBaseViewController: UIViewController {
 //  表格视图，因此不能用懒加载
     var tableView: UITableView?
@@ -46,6 +48,11 @@ extension HWBBaseViewController{
         
 //        view.addSubview(tableView!)
         view.insertSubview(tableView!, belowSubview: navigationBar)
+        
+//        设置数据源和代码方法 ->让自类直接实现数据源方法
+        tableView?.delegate = self
+        tableView?.dataSource = self
+        
     }
     
     private func setupNavigationBar(){
@@ -57,6 +64,19 @@ extension HWBBaseViewController{
         navigationBar.barTintColor = UIColor.cz_color(withHex: 0xF6F6F6)
         navigationBar.titleTextAttributes = [NSForegroundColorAttributeName:UIColor.darkGray]
         navigationBar.isTranslucent = false;
+    }
+}
+
+//遵守协议
+extension HWBBaseViewController:UITableViewDelegate,UITableViewDataSource{
+    
+    //基类准备方法，子类不需要继承，重写方法就可以了
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
 
